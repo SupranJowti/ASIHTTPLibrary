059fa9a42f39dd6727c20d6a44b62b912bd828c3
Supran <>
2020-07-20 18:49:57 +0900

    code

---
+++ ASIHTTPLibrary/Classes/Dummy.h
@@ -0,0 +1,16 @@
+//
+//  Dummy.h
+//  ASIHTTPLibrary
+//
+//  Created by Supran on 7/20/20.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Dummy : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ASIHTTPLibrary/Classes/Dummy.m
@@ -0,0 +1,12 @@
+//
+//  Dummy.m
+//  ASIHTTPLibrary
+//
+//  Created by Supran on 7/20/20.
+//
+
+#import "Dummy.h"
+
+@implementation Dummy
+
+@end
